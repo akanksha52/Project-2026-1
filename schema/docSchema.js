55f1890e911cd5c88010eefcdfc7534a357e3a21
Akanksha Gangrade <akankshagangrade27@gmail.com>
2026-04-03 02:48:03 +0530

    Doc schema and model added

---
+++ schema/docSchema.js
@@ -0,0 +1,28 @@
+import mongoose from "mongoose";
+
+const docSchema=new mongoose.Schema(
+{
+    title: 
+    {
+        type: String,
+        required: true, 
+        trim: true,
+        lowercase: true
+    },
+    content: 
+    {
+        type: String,
+        default: ""
+    },
+    owner:
+    {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "user",
+        required: true
+    }},
+    {timestamps: true}
+);
+
+docSchema.index({ owner: 1, title: 1 }, { unique: true });
+
+export default docSchema;
