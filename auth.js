2118770e81b4148bd780a19de82a6fcec447a826
Akanksha Gangrade <akankshagangrade27@gmail.com>
2026-04-03 00:59:03 +0530

    Login routes checked

--- auth.js
+++ auth.js
@@ -1,24 +1,30 @@
 import express from "express";
 import path from "path";
 import mongoose from "mongoose";
+import userModel from "./models/userModel.js";
 
-async function connectToDB()
+const dbName="collab";
+
+async function connectToDB() 
 {
-    while(true)
+    while(true) 
     {
-        await mongoose.connect('mongodb://127.0.0.1:27017/test').then()
+        try 
         {
+            await mongoose.connect(`mongodb://127.0.0.1:27017/${dbName}`);
             console.log("DB connected!");
-            return;
-        }.catch(err)
+            break;
+        } 
+        catch(err) 
         {
-            console.log("Failed to connect to DB");
-        };
+            console.log(err);
+            console.log("Failed to connect. Retrying in 3s...");
+            await new Promise(res => setTimeout(res, 3000));
+        }
     }
-}
+};
 
-const Cat = mongoose.model('Cat', { name: String });
-
+connectToDB();
 
 const app=express();
 
@@ -26,9 +32,41 @@ app.use(express.static("public"));
 app.use(express.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 
+app.get("/", (req, res) =>
+{
+    res.send("Home page!");
+})
+
 app.get("/login", (req, res) =>
 {
     res.render("1_login.ejs");
+});
+
+app.post("/login", async (req, res) =>
+{
+    const {field, password}=req.body;
+    if(!field || !password)
+    {
+        return res.send("Failed to login!")
+    }
+    let result;
+    if(field.includes("@"))
+    {
+        result=await userModel.findOne({email: field});
+    }
+    else
+    {
+        result=await userModel.findOne({phone: field});
+    }
+    if(!result)
+    {
+        return res.send("You are not registered!");
+    }
+    if(password!=result.password)
+    {
+        return res.send("Wrong password!");
+    }
+    res.redirect("/");
 });
 
 app.get("/signup", (req, res) =>
