b90257cde469ae971dc400f653a1563bb660babe
Akanksha Gangrade <akankshagangrade27@gmail.com>
2026-04-10 18:44:12 +0530

    Added try catch in all controllers of documents

--- controllers/docController.js
+++ controllers/docController.js
@@ -7,12 +7,12 @@ export const getAll=async (req, res) =>
 {
     try
     {
-        const page=parseInt(req,Query.page) || 1;
+        const page=parseInt(req.query.page) || 1;
         const lim=10;
         const docs=await docModel.find({owner: req.userId})
                         .select("title createdAt updatedAt")
-                        .skip((page-1)*limit)
-                        .limit(limit)
+                        .skip((page-1)*lim)
+                        .limit(lim)
                         .sort({updatedAt: -1});
         res.json(docs);
     }
@@ -24,10 +24,17 @@ export const getAll=async (req, res) =>
 
 export const getById=async (req, res) =>
 {
-    const doc=await docModel.findById(req.params.id);
-    if(!doc) return res.json({message: "Document not found!"});
-    if(doc.owner.toString()!=req.userId) return res.send("Unauthorised");
-    res.json(doc);
+    try
+    {
+        const doc=await docModel.findById(req.params.id);
+        if(!doc) return res.json({message: "Document not found!"});
+        if(doc.owner.toString()!==req.userId) return res.json({message: "Unauthorised"});
+        res.json(doc); 
+    }
+    catch(e)
+    {
+        return res.status(500).json({message: "Some error occured"});
+    }
 };
 
 export const createDoc=async (req, res) =>
@@ -40,28 +47,42 @@ export const createDoc=async (req, res) =>
     }
     catch(err)
     {
-        if(err.code==11000) return res.send("Title already exists");
+        if(err.code==11000) return res.send({message: "Title already exists"});
         return res.json({message: "Error creating document"});
     }
 }
 
 export const putDocById=async (req, res) =>
 {
-    const {content , title}=req.body;
-    const doc=await docModel.findById(req.params.id);
-    if(!doc) return res.send("Document not found");
-    if(doc.owner.toString!=req.userId) return res.send("Unauthorised");
-    if(content!=undefined) doc.content=content;
-    if(title!=undefined) doc.title=title;
-    await doc.save();
-    res.json({message: "Updated"});
+    try
+    {
+        const {content , title}=req.body;
+        const doc=await docModel.findById(req.params.id);
+        if(!doc) return res.status(404).json({ message: "Document not found" })
+        if(doc.owner.toString()!==req.userId) return res.json({message: "Unauthorised"});
+        if(content!=undefined) doc.content=content;
+        if(title!=undefined) doc.title=title;
+        await doc.save();
+        res.json({message: "Updated"}); 
+    }
+    catch(err)
+    {
+        return res.status(500).json({message: "Some error occured"});
+    }
 }
 
 export const deleteDocById=async (req, res) =>
 {
-    const doc=await docModel.findById(req.params.id);
-    if(!doc) return res.send("Document not found");
-    if(doc.owner.toString!=req.userId) return res.send("Unauthorised");
-    await doc.deleteOne();
-    res.json({message: "Deleted"});
+    try
+    {
+        const doc=await docModel.findById(req.params.id);
+        if(!doc) return res.status(404).json({ message: "Document not found" })
+        if(doc.owner.toString()!==req.userId) return res.json({message: "Unauthorised"});
+        await doc.deleteOne();
+        res.json({message: "Deleted"});
+    }
+    catch(err)
+    {
+        return res.status(500).json({message: "Some error occured"});
+    }
 }
